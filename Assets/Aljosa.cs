78614ce299a1480a2c6d74d45c22b5db258f5528
unknown-user <unknown-user@fe80::3c0f:5b70:e0a9:7a40%16>
2015-03-23 22:34:50 +0100

    dodan

---
+++ Assets/Aljosa.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aljosa : MonoBehaviour {
+
+	// Use this for initialization
+	//ssss
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
