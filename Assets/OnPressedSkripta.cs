4c8f116f3eb9e589bd1d46ba5297b9d4ae1f8864
bohte bostjan <bohtebostjan@gmail.com>
2015-03-24 17:46:57 +0100

    komitam level 10

---
+++ Assets/OnPressedSkripta.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnPressedSkripta : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
