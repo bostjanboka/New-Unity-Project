93f4f69eb3e6911467d832dd509de41d3ee013e7
kraljo <aljosa@mordenkul.com>
2015-03-27 18:14:24 +0100

    test

---
+++ Assets/MuteMusic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuteMusic : MonoBehaviour {
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space))
+			if (GetComponent<AudioSource>().mute)
+				GetComponent<AudioSource>().mute = false;
+		else
+			GetComponent<AudioSource>().mute = true;
+		
+	}
+}
