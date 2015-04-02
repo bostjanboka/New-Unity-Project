d9c84e2390cc08db521b9ad1c794add6a1736b22
bohte bostjan <bohtebostjan@gmail.com>
2015-04-03 00:17:00 +0200

    mute zvok in melodijo

--- Assets/DontDestroyOnLoad.cs
+++ Assets/DontDestroyOnLoad.cs
@@ -3,7 +3,28 @@ using System.Collections;
 
 public class DontDestroyOnLoad : MonoBehaviour {
 
+
+	AudioSource audio;
+	GameObject gumbMute;
+	GameObject MuzikaMute;
+	bool zvok = false;
+	bool muzika = false;
 	void Awake() {
 		DontDestroyOnLoad(transform.gameObject);
+		audio = gameObject.GetComponent<AudioSource> ();
+	}
+
+	public void muteZvok(){
+		zvok = !zvok;
+		if (zvok) {
+			AudioListener.volume = 0;
+		} else {
+			AudioListener.volume=1;
+		}
+	}
+
+	public void muteMuzika(){
+		muzika = !muzika;
+		audio.mute = muzika;
 	}
 }
