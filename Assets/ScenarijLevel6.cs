6317c57f442a3fbbd3edbe6ee06fbb7090911347
bohte bostjan <bohtebostjan@gmail.com>
2015-04-09 16:30:16 +0200

    level6

---
+++ Assets/ScenarijLevel6.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScenarijLevel6 : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject junak;
+	NewBehaviourScript junakSkripta;
+	public GameObject palcek;
+	PalcekAI palcekSkripta;
+	
+	
+	
+	public GameObject prostorZogic;
+	public GameObject zmagal;
+	public GameObject akcija;
+	SteviloZogicSkripta steviloZogic;
+	
+	int stanje;
+	void Start () {
+		junakSkripta = junak.GetComponent<NewBehaviourScript> ();
+		
+		steviloZogic = prostorZogic.GetComponent<SteviloZogicSkripta> ();
+
+		palcekSkripta = palcek.GetComponent<PalcekAI> ();
+		palcekSkripta.xTocka = 7f;
+		junakSkripta.xTocka = 0f;
+		stanje = 0;
+		akcija.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (stanje == 0 && palcekSkripta.stojimNaMestuX) {
+			stanje = 1;
+			akcija.SetActive (true);
+
+			palcekSkripta.xTocka=12f;
+			
+		}else if(stanje == 1 && steviloZogic.prazenProstor){
+			zmagal.SetActive(true);
+			Time.timeScale=0;
+		}
+		
+	}
+}
