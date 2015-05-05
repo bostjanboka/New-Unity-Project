1e32eae73efec0ad4df5da2fd0fc2b28f55d9e35
bohte bostjan <bohtebostjan@gmail.com>
2015-05-05 23:58:32 +0200

    se malo

---
+++ Assets/RankUserjaSkripta.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RankUserjaSkripta : MonoBehaviour {
+
+	// Use this for initialization
+
+	void OnGUI() // simply an example of a long ScrollView
+	{
+		if (userService.playerName != null) {
+			gameObject.GetComponent<Text>().text = userService.rankUserja;
+		}
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
