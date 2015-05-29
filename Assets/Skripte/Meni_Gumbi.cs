3756eb1aee852e04371a223151bfb21401da9025
bostjanboka <bohtebostjan@gmail.com>
2015-05-29 13:40:01 +0200

    meni

--- Assets/Skripte/Meni_Gumbi.cs
+++ Assets/Skripte/Meni_Gumbi.cs
@@ -6,11 +6,12 @@ using UnityEngine.UI;
 
 public class Meni_Gumbi : MonoBehaviour {
 
-	// Use this for initialization
-	public Camera meni;
-	public Camera potka;
-	public Camera input;
-	public Camera scori;
+
+	public GameObject meni;
+	public GameObject scori;
+	public GameObject potka;
+	public GameObject input;
+
 
 	public InputField app42InputNick;
 
@@ -28,95 +29,42 @@ public class Meni_Gumbi : MonoBehaviour {
 	GameObject oglasi;
 
 	GameObject userSer;
-	public GameObject gameMusic;
-	public GameObject loadingMorden;
-	public GameObject loadingMorden1;
-	public GameObject loadingGame;
-	public GameObject loadingSign;
+
 
 	public GameObject popUpRate;
-	public GameObject user;
+
 
 	public Toggle musicToggle; 
 	public Toggle soundToggle; 
 
 	public static int stKamere=0;
-	public Button exit;
-	public GameObject Oglasi;
+	
 
 
-
-	public Button continueGameGumb;
-
-	public Button continueGameMeni;
 
 	public static bool pojdiVMeni=false;
 	GameObject zvok;
 
 	bool casNazaj=false;
 	void Awake(){
-		//PlayerPrefs.DeleteAll ();
 
-		//exit.gameObject.transform.position = meni.ScreenToWorldPoint(new Vector3(Screen.width,Screen.height,100));
-		if (GameObject.Find ("Oglasi(Clone)") == null) {
-			oglasi = Instantiate (Oglasi) as GameObject;
-			oglasi.GetComponent<OglasiSkripta>().initReklamo();
-		} else {
-			oglasi = GameObject.Find ("Oglasi(Clone)");
-		}
+		oglasi = GameObject.Find ("Oglasi");
 
+		zvok = GameObject.Find("game music");
 
-		zvok = GameObject.Find("game music(Clone)");
-		if(zvok == null)
-			zvok = Instantiate (gameMusic) as GameObject;
-		if (GameObject.Find ("Loading mordenkul(Clone)") == null) {
-			mordenLogo = Instantiate (loadingMorden) as GameObject;
-			mordenLogo.GetComponent<LoadingScreen>().show();
-		} else {
-			mordenLogo = GameObject.Find ("Loading mordenkul(Clone)");
-		}
+		mordenLogo = GameObject.Find ("Loading mordenkul");
+			
+		singUpLogo = GameObject.Find ("splash_prozoren");
+			
+		gameLogo = GameObject.Find ("Loading Logo");
 
-		if (GameObject.Find ("splash_prozoren(Clone)") == null) {
-			singUpLogo = Instantiate (loadingSign) as GameObject;
-
-		} else {
-			singUpLogo = GameObject.Find ("splash_prozoren(Clone)");
-		}
-
-		if (GameObject.Find ("Loading mordenkul 1(Clone)") == null) {
-			mordenLogo1 = Instantiate (loadingMorden1) as GameObject;
-			mordenLogo1.GetComponent<LoadingScreen>().show();
-		} else {
-			mordenLogo1 = GameObject.Find ("Loading mordenkul 1(Clone)");
-		}
-
-		if (GameObject.Find ("Loading Logo(Clone)") == null) {
-			gameLogo = Instantiate (loadingGame) as GameObject;
-		} else {
-			gameLogo = GameObject.Find ("Loading Logo(Clone)");
-		}
-
-		if (!GameObject.Find ("User(Clone)")) {
-			userSer = Instantiate (user) as GameObject;
-			if (LeveliManeger._instance.getIdUser () == null) {
-				stKamere=2;
-			}
-		} else {
-			userSer = GameObject.Find ("User(Clone)");
-		}
-
+		userSer = GameObject.Find ("User");
+			
 		musicToggle.isOn = !zvok.GetComponent<DontDestroyOnLoad> ().muzika;
-
 
 		soundToggle.isOn = !zvok.GetComponent<DontDestroyOnLoad> ().zvok;
 
-
 		singUpLogo.SetActive (false);
-
-
-		//zvok.GetComponent<DontDestroyOnLoad> ().muteZvok (!soundToggle.isOn);
-		//zvok.GetComponent<DontDestroyOnLoad> ().muteMuzika (!musicToggle.isOn); ss
-
 
 
 	}
@@ -154,26 +102,10 @@ public class Meni_Gumbi : MonoBehaviour {
 
 
 	void Start () {
-		if (GameObject.Find ("InputField Input Caret")) {
-			GameObject.Find ("InputField Input Caret").SetActive (false);
-		}
+
 		popUpRate.SetActive (false);
 
-
-		kamera(stKamere);
-		InfoLeveli temp = LeveliManeger._instance.getLevel ();
-		
-		if (continueGameGumb && (temp.level == -1 || temp.level % 2 == 0)) {
-			continueGameGumb.interactable = false;
-		} 
-
-		if (continueGameGumb && temp.level % 2 != 0 && temp.score == 0) {
-			continueGameGumb.interactable=false;
-		}
 		int obiski = LeveliManeger._instance.obiskalIgro ();
-		if (continueGameMeni && obiski == 0) {
-			continueGameMeni.interactable=false;
-		}
 
 		if (LeveliManeger._instance.pokaziRate() == 1 && obiski > 0) {
 			popUpRate.SetActive(true);
@@ -185,8 +117,8 @@ public class Meni_Gumbi : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Escape)) {
-			if(potka.enabled){
-				kamera(0);
+			if(!meni.activeSelf){
+				canvas(0);
 			}else{
 				LeveliManeger._instance.povecajObisk();
 				Application.Quit();
@@ -194,7 +126,7 @@ public class Meni_Gumbi : MonoBehaviour {
 
 		}
 
-		if (input.enabled && singUpLogo.activeSelf) {
+		if (input.activeSelf && singUpLogo.activeSelf) {
 
 			Time.timeScale = 0.0000001f;
 		} else if(casNazaj){
@@ -203,11 +135,10 @@ public class Meni_Gumbi : MonoBehaviour {
 		}
 		if (pojdiVMeni) {
 			pojdiVMeni=false;
-			if(input.enabled){
-				kamera(0);
+			if(input.activeSelf){
+				canvas(0);
 				singUpLogo.SetActive(false);
 				casNazaj=true;
-
 			}
 
 		}
@@ -234,13 +165,11 @@ public class Meni_Gumbi : MonoBehaviour {
 	}
 
 	public void naloziZemljevid(){
-		potka.enabled = true;
-		meni.enabled = false;
+		canvas (1);
 	}
 
 	public void naloziMeni(){
-		potka.enabled = false;
-		meni.enabled=true;
+		canvas (0);
 	}
 
 	public void newGameButton(){
@@ -248,23 +177,9 @@ public class Meni_Gumbi : MonoBehaviour {
 		gameLogo.GetComponent<LoadingScreen> ().show ();
 	}
 
-	public void continueGame(){
-		InfoLeveli temp = LeveliManeger._instance.getLevel ();
 
-		if ((temp.level == -1 || temp.level % 2 == 0) || temp.level % 2 != 0 && temp.score == 0) {
-			kamera(1);
-		} else {
-			if (temp.level % 2 == 0) {
-				Application.LoadLevel ("level" + temp.level * 2); 
-			} else {
-				Application.LoadLevel ("level"+(temp.level*2-1+temp.trenutniLevel)); 
-			}
-			mordenLogo.GetComponent<LoadingScreen> ().show ();
-		}
-	}
 
 	public void gumbRateX(){
-		Debug.Log ("gumb rate X");
 		popUpRate.SetActive (false);
 		LeveliManeger._instance.disableRate ();
 	}
@@ -308,19 +223,19 @@ public class Meni_Gumbi : MonoBehaviour {
 
 	}
 
-	public void kamera(int i){
-		meni.enabled = false;
-		potka.enabled = false;
-		scori.enabled = false;
-		input.enabled = false;
+	public void canvas(int i){
+		meni.SetActive (false);
+		potka.SetActive (false);
+		scori.SetActive (false);
+		input.SetActive (false);
 		if (i == 0) {
-			meni.enabled = true;
+			meni.SetActive (true);
 		} else if (i == 1) {
-			potka.enabled = true;
+			potka.SetActive (true);
 		} else if (i == 2) {
-			input.enabled = true;
+			input.SetActive (true);
 		} else if (i == 3) {
-			scori.enabled=true;
+			scori.SetActive (true);
 		}
 	}
 
